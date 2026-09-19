b8eb4a3fe2f03b000d7b8b77f0b43e12667f1b75
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-09-18 22:04:11 -0300

    first commit

---
+++ src/main/kotlin/com/viniciusdevassis/laumileymodas/LaumileyModasApiApplication.kt
@@ -0,0 +1,11 @@
+package com.viniciusdevassis.laumileymodas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaumileyModasApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LaumileyModasApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/viniciusdevassis/laumileymodas/LaumileyModasApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.laumileymodas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LaumileyModasApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
